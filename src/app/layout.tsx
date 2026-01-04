163ed8038d745b63bc2988343876cfb12e9e7ce1
KD5000 <kd5000@KD5000s-MacBook-Pro.local>
2026-01-03 20:56:00 -0500

    feat: add theme provider to layout to enable appearance switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthListener } from "@/components/auth-listener";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,9 +31,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <AuthListener />
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <AuthListener />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
